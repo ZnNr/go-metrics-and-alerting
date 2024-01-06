6a26011148147b1144c9366cf73f6d6cf1055094
lightspot <zinnurshamsutdinov@yandex.ru>
2024-01-06 22:23:06 +0300

    iter7 исправлен хендлер

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -13,15 +13,14 @@ import (
 	"strconv"
 )
 
-// SaveMetric Функция сохранения метрики
 func SaveMetric(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
-	metricType := chi.URLParam(r, "type")   // Получает значение параметра "type" из URL
-	metricName := chi.URLParam(r, "name")   // Получает значение параметра "name" из URL
-	metricValue := chi.URLParam(r, "value") // Получает значение параметра "value" из URL
+	metricType := chi.URLParam(r, "type")
+	metricName := chi.URLParam(r, "name")
+	metricValue := chi.URLParam(r, "value")
 
 	err := collector.Collector.Collect(metricName, metricType, metricValue)
 	if errors.Is(err, collector.ErrBadRequest) {
@@ -36,52 +35,13 @@ func SaveMetric(w http.ResponseWriter, r *http.Request) {
 	if _, err = io.WriteString(w, ""); err != nil {
 		return
 	}
-	w.Header().Set("content-type", "text/plain; charset=utf-8")
-	w.Header().Set("content-length", strconv.Itoa(len(metricName)))
-	w.WriteHeader(http.StatusOK)
-}
-
-// GetMetric Функция получения метрики
-func GetMetric(w http.ResponseWriter, r *http.Request) {
-	metricType := chi.URLParam(r, "type")
-	metricName := chi.URLParam(r, "name")
-
-	value, err := collector.Collector.GetMetricByName(metricName, metricType)
-	if errors.Is(err, collector.ErrNotFound) {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-	if errors.Is(err, collector.ErrNotImplemented) {
-		w.WriteHeader(http.StatusNotImplemented)
-		return
-	}
 
 	if _, err = io.WriteString(w, ""); err != nil {
 		return
 	}
+	io.WriteString(w, "")
 	w.Header().Set("content-type", "text/plain; charset=utf-8")
-	w.Header().Set("content-length", strconv.Itoa(len(value)))
-	w.WriteHeader(http.StatusOK)
-	if _, err = io.WriteString(w, value); err != nil {
-		return
-	}
-}
-
-// ShowMetrics Функция отображения всех метрик
-func ShowMetrics(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.Error(w, fmt.Sprintf("wrong path %q", r.URL.Path), http.StatusNotFound)
-		return
-	}
-	page := ""
-	for _, n := range collector.Collector.GetAvailableMetrics() {
-		page += fmt.Sprintf("<h1>	%s</h1>", n)
-	}
-	tmpl, _ := template.New("data").Parse("<h1>AVAILABLE METRICS</h1>{{range .}}<h3>{{ .}}</h3>{{end}}")
-	if err := tmpl.Execute(w, collector.Collector.GetAvailableMetrics()); err != nil {
-		return
-	}
-	w.Header().Set("content-type", "Content-Type: text/html; charset=utf-8")
+	w.Header().Set("content-length", strconv.Itoa(len(metricName)))
 	w.WriteHeader(http.StatusOK)
 }
 
@@ -160,6 +120,7 @@ func SaveMetricFromJSON(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-length", strconv.Itoa(len(metric.ID)))
 	w.WriteHeader(http.StatusOK)
 }
+
 func GetMetricFromJSON(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
 	var buf bytes.Buffer
@@ -216,6 +177,49 @@ func GetMetricFromJSON(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("content-length", strconv.Itoa(len(value)))
 	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+}
+
+func GetMetric(w http.ResponseWriter, r *http.Request) {
+	metricType := chi.URLParam(r, "type")
+	metricName := chi.URLParam(r, "name")
+
+	value, err := collector.Collector.GetMetricByName(metricName, metricType)
+	if errors.Is(err, collector.ErrNotFound) {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	if errors.Is(err, collector.ErrNotImplemented) {
+		w.WriteHeader(http.StatusNotImplemented)
+		return
+	}
+
+	if _, err = io.WriteString(w, ""); err != nil {
+		return
+	}
+	io.WriteString(w, "")
+	w.Header().Set("content-type", "text/plain; charset=utf-8")
+	w.Header().Set("content-length", strconv.Itoa(len(value)))
+	w.WriteHeader(http.StatusOK)
+	if _, err = io.WriteString(w, value); err != nil {
+		return
+	}
+}
+
+func ShowMetrics(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, fmt.Sprintf("wrong path %q", r.URL.Path), http.StatusNotFound)
+		return
+	}
+	page := ""
+	for _, n := range collector.Collector.GetAvailableMetrics() {
+		page += fmt.Sprintf("<h1>	%s</h1>", n)
+	}
+	tmpl, _ := template.New("data").Parse("<h1>AVAILABLE METRICS</h1>{{range .}}<h3>{{ .}}</h3>{{end}}")
+	if err := tmpl.Execute(w, collector.Collector.GetAvailableMetrics()); err != nil {
+		return
+	}
+	w.Header().Set("content-type", "Content-Type: text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 }
 
